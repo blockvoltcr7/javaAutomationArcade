2d1777406a4ea0abd3cad72b303f64d71cd84faa
Sami Sabir-Idrissi <sami_sabir-idrissi@epam.com>
2024-03-26 05:04:36 -0400

    report portal tests are working and sending results to reportportal

--- src/test/java/ReportPortalTestNGSmoke/ReportPortalSmokeTest.java
+++ src/test/java/ReportPortalTestNGSmoke/ReportPortalSmokeTest.java
@@ -3,13 +3,21 @@ package ReportPortalTestNGSmoke;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class reportportalhelloworld {
+public class ReportPortalSmokeTest {
 
 
 
     @Test
     public void helloWorldTest() {
         String hello = "Hello, world!";
+        System.out.println(hello);
         Assert.assertEquals(hello, "Hello, world!");
+    }
+
+    @Test
+    public void failingOnPurpose() {
+        String hello = "Hello, world!";
+        System.out.println(hello);
+        Assert.assertEquals(hello, "Nope its another galaxy!");
     }
 }
